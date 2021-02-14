d9b35d2675cb61fcbb2f4fd86315392ff5dddf76
chama0 <chama_zer0@yahoo.co.jp>
2021-02-14 22:27:06 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/ }
+  validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ }
+  validates :password_confirm, presence: true, length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ }
+  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :lastname_reading, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ }
+  validates :firstname_reading, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ }
+  validates :birthday, presence: true
 end
