fcec36c6921b42d69247b0db0e92fe5242123073
chama0 <chama_zer0@yahoo.co.jp>
2021-02-18 17:11:07 +0900

    FactoryBot生成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name        { Faker::Name.name }
+    description { Faker::Lorem.sentence }
+    category    { Category.all.sample }
+    status      { Status.all.sample }
+    shipfee     { Shipfee.all.sample }
+    prefecture  { Prefecture.all.sample }
+    estshipdate { Estshipdate.all.sample }
+    price       { "1000" }
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
