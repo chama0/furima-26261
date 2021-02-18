bbb5229fb5cbdb467cb0f1ed495610a03f6f27a0
chama0 <chama_zer0@yahoo.co.jp>
2021-02-18 16:34:21 +0900

    indexのパス設定とバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
     validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { less_than_or_equal_to: 9999999, greater_than_or_equal_to: 300 }
     validates :image
 
-    with_options numericality: { other_than: 1 }
+    with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :status_id
       validates :shipfee_id
