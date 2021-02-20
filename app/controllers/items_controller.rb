81eea9ea754c03e18fae84c370e996f927055f00
chama0 <chama_zer0@yahoo.co.jp>
2021-02-20 17:14:29 +0900

    itemsコントローラー設定の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @item.update(item_params)
+    if user_signed_in? && current_user.id != @item.update(item_params)
       redirect_to item_path
     else
       render :edit
