b2c6874f62f3d8ca544ddad2bcde72f4686f604b
chama0 <chama_zer0@yahoo.co.jp>
2021-02-24 03:55:14 +0900

    purchasesコントローラー設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,23 @@
 class PurchasesController < ApplicationController
 
   def index
+    @purchase_address = PurchaseAddress.new
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
-  
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:zipcode, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number)
+  end
+
 end
