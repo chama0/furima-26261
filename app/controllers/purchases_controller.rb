a4ceaae759b37650d0b2cb45840a80827f6b7967
chama0 <chama_zer0@yahoo.co.jp>
2021-02-24 04:52:28 +0900

    purchasesコントローラー設定、ビューファイル編集(商品名、販売価格)

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,5 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
     @purchase_address = PurchaseAddress.new
@@ -18,6 +19,10 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_address).permit(:zipcode, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
