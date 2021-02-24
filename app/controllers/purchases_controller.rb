2b184fc2ec3aa54ae47a4d9b8113be9d19ea836b
chama0 <chama_zer0@yahoo.co.jp>
2021-02-24 09:30:13 +0900

    サーバーサイド実装(決済処理)

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,8 +6,9 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    @purchase_address = PurchaseAddress.new(purchase_params)
+    @purchase_address = PurchaseAddress.new(purchase_address_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -17,12 +18,21 @@ class PurchasesController < ApplicationController
 
   private
 
-  def purchase_params
+  def purchase_address_params
     params.require(:purchase_address).permit(:zipcode, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_address_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
